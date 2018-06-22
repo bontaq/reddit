bc7d5b3bc56cde28193dadfb85f447ccef48c309
Ian Davidson <bontaq@gmail.com>
2018-06-22 01:01:52 -0400

    Use env vars

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import           System.Environment     (getEnv)
 import           Control.Lens
 import           Control.Monad.IO.Class
 import           Data.Aeson             (decode)
@@ -13,22 +14,29 @@ import           Data.Text.Encoding     (encodeUtf8)
 import           GHC.Word
 import           Network.Wreq
 
-clientSecret = ""
-clientId     = ""
-tokenUrl     = "https://www.reddit.com/api/v1/access_token"
+tokenUrl = "https://www.reddit.com/api/v1/access_token"
 
 getHot :: Options -> String -> IO (Response LBS.ByteString)
 getHot opts subreddit = getWith opts ("https://oauth.reddit.com/r/" ++ subreddit ++ "/hot")
 
 main :: IO ()
 main = do
-  let opts = defaults & auth ?~ basicAuth clientId clientSecret
-  r <- postWith opts tokenUrl [ ("grant_type" :: B.ByteString) := ("password"     :: B.ByteString)
-                              , ("username"   :: B.ByteString) := ("quakquakquak" :: B.ByteString)
-                              , ("password"   :: B.ByteString) := (""      :: B.ByteString) ]
+  -- env
+  user         <- getEnv "REDDIT_USER_NAME"
+  password     <- getEnv "REDDIT_USER_PASSWORD"
+  clientSecret <- getEnv "REDDIT_CLIENT_SECRET"
+  cliendId     <- getEnv "REDDIT_CLIENT_ID"
+
+  print user
+  -- token
+  let opts = defaults & auth ?~ basicAuth (pack cliendId) (pack clientSecret)
+  r <- postWith opts tokenUrl [ ("grant_type" :: B.ByteString) := ("password"      :: B.ByteString)
+                              , ("username"   :: B.ByteString) := ("quakquakquak"  :: B.ByteString)
+                              , ("password"   :: B.ByteString) := ((pack password) :: B.ByteString) ]
   let token = r ^. responseBody . key "access_token" . _String
       authOpts = defaults & auth ?~ oauth2Bearer (encodeUtf8 token)
 
+  -- do
   res <- getHot authOpts "the_donald"
   print $ res ^. responseBody
 
